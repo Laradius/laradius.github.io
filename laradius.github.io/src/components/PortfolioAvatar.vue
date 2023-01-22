81011fc82c9ffe9af997fc799142de541d24fe37
Elliot White <elliot.white@petraagency.pl>
2023-01-22 19:24:43 +0100

    Latest changes

---
+++ laradius.github.io/src/components/PortfolioAvatar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="headline">
+    <h3 class="text-h3">Welcome to portfolio of</h3>
+    <h1 class="text-h1">Elliot White</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.headline {
+  margin: 2rem 0;
+}
+
+h1 {
+  font-family: verve;
+  position: relative;
+  padding: 0 2rem;
+}
+
+h3 {
+  font-family: calluna;
+  position: relative;
+
+  padding: 0 2rem;
+
+  &::before {
+    position: absolute;
+    content: '';
+    height: 5px;
+    width: 50%;
+    top: 50%;
+    left: 0;
+    transform: translate(-100%, -50%);
+    border: 0;
+    border-top: 5px solid $hr-color;
+  }
+
+  &::after {
+    position: absolute;
+    content: '';
+    height: 5px;
+    width: 50%;
+    top: 50%;
+    right: 0;
+    transform: translate(100%, -50%);
+    border: 0;
+    border-top: 5px solid $hr-color;
+  }
+}
+</style>
--- laradius.github.io/src/pages/IndexPage.vue
+++ laradius.github.io/src/pages/IndexPage.vue
@@ -1,17 +1,18 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="row justify-evenly">
+    <div>
+      <div class="col"><PortfolioText /></div>
+      <div class="col">
+        <PortfolioAvatar />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import PortfolioText from 'src/components/PortfolioText.vue';
+import PortfolioAvatar from 'src/components/PortfolioAvatar.vue';
 import { ref } from 'vue';
 
 const todos = ref<Todo[]>([
